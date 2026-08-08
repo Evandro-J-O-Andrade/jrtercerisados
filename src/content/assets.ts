615f9a1ab634e59c40383077aca722068579e778
Evandro Andrade <evandro_j.o.a@hotmail.com>
2026-08-08 04:15:02 -0300

    feat(assets): cria mapa central de imagens de serviços

---
+++ src/content/assets.ts
@@ -0,0 +1,25 @@
+export const SERVICE_IMAGES = {
+  assessoriaRh: '/images/services/assessoria-rh.png',
+  facilities: '/images/services/facilities-alt.jfif',
+  facilitiesFallback: '/images/services/facilities.png',
+  recrutamento: '/images/services/recrutamento-alt.jfif',
+  limpeza: '/images/services/limpeza-escritorio.jfif',
+  jardinagem: '/images/services/jardinagem.svg',
+  maoDeObraTemporaria: '/images/services/temporaria.svg',
+  maoDeObraEfetiva: '/images/services/efetiva.svg',
+  terceirizacao: '/images/services/seguranca-patrimonial.svg',
+  controleAcesso: '/images/services/controle-acesso.jfif',
+  solucaoRh: '/images/services/solucao-rh.jfif',
+  timeRh: '/images/team/time-rh.jfif',
+} as const;
+
+export const CANDIDATE_IMAGES = {
+  cadastroCurriculo: '/images/candidates/cadastro-curriculo.png',
+  buscaVagas: '/images/candidates/busca-vagas.png',
+} as const;
+
+export const FALLBACK_IMAGES = {
+  servicos: '/images/fallbacks/servicos.png',
+  vagas: '/images/fallbacks/vagas.png',
+  contato: '/images/fallbacks/contato.png',
+} as const;
